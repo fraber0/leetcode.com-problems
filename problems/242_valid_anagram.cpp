35fec25087ad22ef2653955bd67d72ed20b58781
fraber0 <126325929+fraber0@users.noreply.github.com>
2023-03-03 18:29:09 +0100

    Create 242_valid_anagram.cpp

---
+++ problems/242_valid_anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s==t;
+        
+        /*if(s.length()!=t.length())
+        {
+            return false;
+        }
+
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s[i]!=t[i])
+            {
+                return false;
+            }
+        }
+        return true;*/
+    }
+};
